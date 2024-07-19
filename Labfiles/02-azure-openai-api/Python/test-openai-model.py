3e6abb38da0b388ac2a517525004c988fc62e83a
Witali <witali.simon@outlook.com>
2024-07-19 20:40:37 +0200

    resolved

--- Labfiles/02-azure-openai-api/Python/test-openai-model.py
+++ Labfiles/02-azure-openai-api/Python/test-openai-model.py
@@ -1,23 +1,31 @@
 import os
 from dotenv import load_dotenv
-
-# Add Azure OpenAI package
-
+# 1. Add Azure OpenAI package
+from openai import AzureOpenAI
 
 def main(): 
         
     try: 
-    
-        # Get configuration settings 
+        # 2. Get configuration settings 
         load_dotenv()
         azure_oai_endpoint = os.getenv("AZURE_OAI_ENDPOINT")
         azure_oai_key = os.getenv("AZURE_OAI_KEY")
         azure_oai_deployment = os.getenv("AZURE_OAI_DEPLOYMENT")
         
-        # Initialize the Azure OpenAI client...
+        # 3. Initialize the Azure OpenAI client...
+        client = AzureOpenAI(
+            azure_endpoint=azure_oai_endpoint,
+            api_key=azure_oai_key,
+            api_version="2024-02-15-preview"
+        )
+
+        # 4. create system message
+        system_message="""I am a hiking enthusiast named Forest who helps people discover hikes in their area. 
+            If no area is specified, I will default to near Rainier National Park. 
+            I will then provide three suggestions for nearby hikes that vary in length. 
+            I will also share an interesting fact about the local nature on the hikes when making a recommendation.
+            """
         
-
-
         while True:
             # Get input text
             input_text = input("Enter the prompt (or type 'quit' to exit): ")
@@ -29,11 +37,21 @@ def main():
 
             print("\nSending request for summary to Azure OpenAI endpoint...\n\n")
             
-            # Add code to send request...
-            
-            
-            
+            # 5. Add code to send request...
+            response = client.chat.completions.create(
+                model=azure_oai_deployment,
+                temperature=0.5,
+                max_tokens=1000,
+                messages=[
+                    {"role": "system", "content": system_message},
+                    {"role": "user", "content": input_text}
+                ]
+            )
+            generated_text = response.choices[0].message.content
 
+            # Print the respone 
+            print("Response: " + generated_text + "\n")
+            
     except Exception as ex:
         print(ex)
 
